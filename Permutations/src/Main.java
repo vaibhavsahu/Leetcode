6fca8ce05a5ba9eea5b8a94c667cb981788fbf25
Vaibhav Sahu <vsahu@overstock.com>
2021-01-11 22:53:28 -0700

    Change the code to use ArrayList instead of Arrays

--- Permutations/src/Main.java
+++ Permutations/src/Main.java
@@ -1,58 +1,61 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 
 
 public class Main {
     public static void main(String[] args) {
-        int [] arrays = {1, 2, 3, 4};
-       permutations(arrays);
+        int [] arrays = {1, 2, 3, 4,5};
+        System.out.println(permute(arrays));
     }
 
-    private static int [][] insertNumberInArray(int[] arrays, int num) {
+    private static List<List<Integer>> insertNumberInArray(int[] arrays, int num, List<List<Integer>> list) {
         int len = arrays.length+1;
         int len1 = factorial(len);
         int index = 0;
-        int [][] arrayOfArrays = new int[len1][len];
-        while(index < len1) {
-            for (int j = 0; j < len; j++) {
-                int[] arr = insertAtIndexInArray(j, arrays, num);
-                arrayOfArrays[index] = arr;
-                index++;
+        List<List<Integer>> result = new ArrayList<>();
+        if(list.isEmpty()){
+            result.add(Arrays.asList(num));
+        } else {
+            for(int k = 0; k < list.size(); k++){
+                List<Integer> list1 = null;
+                for (int j = 0; j < len; j++) {
+                    list1 = insertAtIndexInList(j, list.get(k), num);
+                    result.add(index, list1);
+                    index++;
+                }
             }
         }
-
-        return arrayOfArrays;
+        return result;
     }
 
-    private static int[] insertAtIndexInArray(int i, int[] arrays, int num) {
-        int [] arr = new int[arrays.length+1];
-        arr[i] = num;
+    private static List<Integer> insertAtIndexInList(int i, List<Integer> list, int num) {
+        List<Integer> result = new ArrayList<>();
         int index = 0;
 
-        for (int j = 0; j <= arrays.length; j++) {
+        for (int j = 0; j <= list.size(); j++) {
             if(j != i){
-                arr[j] = arrays[index++];
+                result.add(j, list.get(index++));
             } else {
-                arr[j] = num;
+                result.add(i, num);
             }
         }
-        return arr;
+        return result;
     }
 
-    private static void permutations(int[] arr) {
+    private static List<List<Integer>> permute(int[] arr) {
 
         if(arr.length == 0){
-             return;
+             return new ArrayList<>();
         }
 
         int first = arr[0];
         int [] rest =  Arrays.copyOfRange(arr, 1, arr.length);
-        permutations(rest);
-        int cols = rest.length+1;
-        int rows = factorial(cols);
-        int [][] arrays = new int[rows][cols];
-        arrays = insertNumberInArray(rest, first);
-        System.out.println(Arrays.deepToString(arrays));
+        List<List<Integer>> list = permute(rest);
+        List<List<Integer>> listList = insertNumberInArray(rest, first, list);
+        return listList;
     }
 
     private static int factorial(int n) {
